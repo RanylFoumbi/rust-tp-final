37acf49304a824815fad42ee8cde6685f9bca73c
Rosvadle MANFO <florientmanfo3@outlook.fr>
2025-02-18 21:25:13 +0100

    Feat: Now we can display map in window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod map;
+mod window;
+
+use map::Map;
+use window::open_window;
+
 fn main() {
-    println!("Hello, world!");
+    let map = Map::new(80, 40, 5);
+    open_window(map).unwrap();
 }
